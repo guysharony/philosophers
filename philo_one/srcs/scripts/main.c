e918d614647dc513a67a6cf2c5e06658ed4c39e6
sharonyguy <sharonyguy@gmail.com>
2020-10-02 11:09:58 +0200

    Test 108

--- philo_one/srcs/scripts/main.c
+++ philo_one/srcs/scripts/main.c
@@ -43,6 +43,5 @@ int     main(int argc, char **argv)
         return (clean(philo_one) && err("A problem occured during malloc.", 0));
     if (thr(philo_one))
         return (1);
-    while (1);
     return (0);
 }
