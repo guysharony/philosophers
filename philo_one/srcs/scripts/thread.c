cfc95160ec1d1b7d2a31320edaed529c59faaca0
guysharony <sharonyguy@gmail.com>
2020-10-03 09:29:52 +0200

    Improving timing

--- philo_one/srcs/scripts/thread.c
+++ philo_one/srcs/scripts/thread.c
@@ -61,7 +61,7 @@ int         thr(t_philo_one *philo_one)
         {
             time = ft_time();
             if (philo_one->params->nw_eat > 0 && 
-            (!philo_one->philo[i]->eat && philo_one->philo[i]->last + philo_one->params->tm_to_die < time))
+            (!philo_one->philo[i]->eat && philo_one->philo[i]->last + philo_one->params->tm_to_die <= time))
             {
                 msg(philo_one->philo[i], "is dead.");
                 philo_one->params->nw_eat = 0;
