0fa893c9562a3609d734aefb4dee2eb1e6857332
guysharony <sharonyguy@gmail.com>
2020-10-03 14:19:56 +0200

    Fixing philo_two

--- philo_two/srcs/scripts/init.c
+++ philo_two/srcs/scripts/init.c
@@ -5,8 +5,8 @@ int             init_options(t_philo_two *tmp, int argc, char **argv)
     if (!(tmp->params = malloc(sizeof(t_params))))
         return (1);
     tmp->params->nb_of_philosophers = ft_atoi(argv[1]);
-    sem_unlink("/fork");
-    if ((tmp->params->fork = sem_open("/fork", O_CREAT, S_IRWXU, tmp->params->nb_of_philosophers)) < 0)
+    sem_unlink("fork");
+    if ((tmp->params->fork = sem_open("fork", O_CREAT, S_IRWXU, tmp->params->nb_of_philosophers)) < 0)
         return (1);
     tmp->params->nw_eat = tmp->params->nb_of_philosophers;
     tmp->params->tm_to_die = ft_atoi(argv[2]);
