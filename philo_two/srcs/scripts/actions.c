835341b1dacb4806ea38d9ce0a0bfc0436f65233
guysharony <sharonyguy@gmail.com>
2020-10-05 15:32:31 +0200

    Test

--- philo_two/srcs/scripts/actions.c
+++ philo_two/srcs/scripts/actions.c
@@ -4,12 +4,12 @@ int     aeat(t_philos *tmp)
 {
     sem_wait(tmp->fork);
     sem_wait(tmp->fork);
-    tmp->eat = 1;
     sem_wait(tmp->write);
     msg(tmp, "has taken a fork.");
     msg(tmp, "has taken a fork.");
     msg(tmp, "is eating.");
     sem_post(tmp->write);
+    tmp->eat = 1;
     tmp->ceat++;
     tmp->last = ft_time();
     usleep(tmp->params->tm_to_eat * 1000);
