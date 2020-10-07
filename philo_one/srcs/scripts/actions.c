e600d5868e45a43f36860f20cf44d6b195111213
guysharony <sharonyguy@gmail.com>
2020-10-07 13:17:35 +0200

    Fixing norms actions.c and init.c

--- philo_one/srcs/scripts/actions.c
+++ philo_one/srcs/scripts/actions.c
@@ -1,35 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guysharony <guysharony@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/07 08:14:27 by gsharony          #+#    #+#             */
+/*   Updated: 2020/10/07 13:01:38 by guysharony       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo_one.h"
 
-int         aeat(t_philos *tmp)
+int			aeat(t_philos *tmp)
 {
-    pthread_mutex_lock(tmp->rfork);
-    pthread_mutex_lock(tmp->lfork);
-    pthread_mutex_lock(tmp->params->write);
-    msg(tmp, "has taken a fork.");
-    msg(tmp, "has taken a fork.");
-    msg(tmp, "is eating.");
-    pthread_mutex_unlock(tmp->params->write);
-    tmp->eat = 1;
-    tmp->ceat++;
-    tmp->last = ft_time();
-    usleep(tmp->params->tm_to_eat * 1000);
-    tmp->eat = 0;
-    pthread_mutex_unlock(tmp->rfork);
-    pthread_mutex_unlock(tmp->lfork);
-    return (tmp->stop);
+	pthread_mutex_lock(tmp->rfork);
+	pthread_mutex_lock(tmp->lfork);
+	pthread_mutex_lock(tmp->params->write);
+	msg(tmp, "has taken a fork.");
+	msg(tmp, "has taken a fork.");
+	msg(tmp, "is eating.");
+	pthread_mutex_unlock(tmp->params->write);
+	tmp->eat = 1;
+	tmp->ceat++;
+	tmp->last = ft_time();
+	usleep(tmp->params->tm_to_eat * 1000);
+	tmp->eat = 0;
+	pthread_mutex_unlock(tmp->rfork);
+	pthread_mutex_unlock(tmp->lfork);
+	return (tmp->stop);
 }
 
-void        asleep(t_philos *tmp)
+void		asleep(t_philos *tmp)
 {
-    pthread_mutex_lock(tmp->params->write);
-    msg(tmp, "is sleeping.");
-    pthread_mutex_unlock(tmp->params->write);
-    usleep(tmp->params->tm_to_sleep * 1000);
+	pthread_mutex_lock(tmp->params->write);
+	msg(tmp, "is sleeping.");
+	pthread_mutex_unlock(tmp->params->write);
+	usleep(tmp->params->tm_to_sleep * 1000);
 }
 
-void        athink(t_philos *tmp)
+void		athink(t_philos *tmp)
 {
-    pthread_mutex_lock(tmp->params->write);
-    msg(tmp, "is thinking.");
-    pthread_mutex_unlock(tmp->params->write);
+	pthread_mutex_lock(tmp->params->write);
+	msg(tmp, "is thinking.");
+	pthread_mutex_unlock(tmp->params->write);
 }
--- philo_one/srcs/scripts/init.c
+++ philo_one/srcs/scripts/init.c
@@ -1,68 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guysharony <guysharony@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/07 13:02:59 by guysharony        #+#    #+#             */
+/*   Updated: 2020/10/07 13:16:36 by guysharony       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo_one.h"
 
-int             init_options(t_philo_one *tmp, int argc, char **argv)
+int				init_mutex(t_philo_one *tmp, size_t size)
 {
-    size_t      i;
+	size_t		i;
 
-    i = 0;
-    if (!(tmp->params = malloc(sizeof(t_params))))
-        return (1);
-    if (!(tmp->params->fork = malloc(sizeof(pthread_mutex_t*) * ft_atoi(argv[1]))))
-        return (1);
-    tmp->params->nb_of_philosophers = ft_atoi(argv[1]);
-    tmp->params->nw_eat = tmp->params->nb_of_philosophers;
-    tmp->params->tm_to_die = ft_atoi(argv[2]);
-    tmp->params->tm_to_eat = ft_atoi(argv[3]);
-    tmp->params->tm_to_sleep = ft_atoi(argv[4]);
-    if (!(tmp->params->write = malloc(sizeof(pthread_mutex_t))))
-        return (1);
-    pthread_mutex_init(tmp->params->write, NULL);
-    while (i < tmp->params->nb_of_philosophers)
-    {
-        if (!(tmp->params->fork[i] = malloc(sizeof(pthread_mutex_t))))
-            return (1);
-        pthread_mutex_init(tmp->params->fork[i], NULL);
-        i++;
-    }
-    tmp->params->nb_eat_philo = -1;
-    if (argc == 6)
-        tmp->params->nb_eat_philo = ft_atoi(argv[5]);
-    return (0);
+	i = 0;
+	if (!(tmp->params->fork = malloc(sizeof(pthread_mutex_t*) * size)))
+		return (1);
+	if (!(tmp->params->write = malloc(sizeof(pthread_mutex_t))))
+		return (1);
+	pthread_mutex_init(tmp->params->write, NULL);
+	while (i < size)
+	{
+		if (!(tmp->params->fork[i] = malloc(sizeof(pthread_mutex_t))))
+			return (1);
+		pthread_mutex_init(tmp->params->fork[i], NULL);
+		i++;
+	}
+	return (0);
 }
 
-int             init_philos(t_philo_one *philo_one, int size)
+int				init_options(t_philo_one *tmp, int argc, char **argv)
 {
-    int         i;
-
-    i = 0;
-    if (!(philo_one->philo = malloc(sizeof(t_philos*) * size)))
-        return (1);
-    while (i < size)
-    {
-        if (!(philo_one->philo[i] = malloc(sizeof(t_philos))))
-            return (1);
-        philo_one->philo[i]->id = i + 1;
-        philo_one->philo[i]->rfork = philo_one->params->fork[i];
-        philo_one->philo[i]->lfork = philo_one->params->fork[((i + 1) % size)];
-        philo_one->philo[i]->params = philo_one->params;
-        philo_one->philo[i]->ceat = 0;
-        philo_one->philo[i]->last = 0;
-        philo_one->philo[i]->eat = 0;
-        philo_one->philo[i]->stop = 0;
-        i++;
-    }
-    return (0);
+	if (!(tmp->params = malloc(sizeof(t_params))))
+		return (1);
+	init_mutex(tmp, ft_atoi(argv[1]));
+	tmp->params->nb_of_philosophers = ft_atoi(argv[1]);
+	tmp->params->nw_eat = tmp->params->nb_of_philosophers;
+	tmp->params->tm_to_die = ft_atoi(argv[2]);
+	tmp->params->tm_to_eat = ft_atoi(argv[3]);
+	tmp->params->tm_to_sleep = ft_atoi(argv[4]);
+	tmp->params->nb_eat_philo = -1;
+	if (argc == 6)
+		tmp->params->nb_eat_philo = ft_atoi(argv[5]);
+	return (0);
 }
 
-t_philo_one     *init(int argc, char **argv)
+int				init_philos(t_philo_one *philo_one, int size)
 {
-    t_philo_one     *tmp;
+	int			i;
 
-    if (!(tmp = malloc(sizeof(t_philo_one))))
-        return (NULL);
-    if (init_options(tmp, argc, argv))
-        return (NULL);
-    if (init_philos(tmp, tmp->params->nb_of_philosophers))
-        return (NULL);
-    return (tmp);
+	i = 0;
+	if (!(philo_one->philo = malloc(sizeof(t_philos*) * size)))
+		return (1);
+	while (i < size)
+	{
+		if (!(philo_one->philo[i] = malloc(sizeof(t_philos))))
+			return (1);
+		philo_one->philo[i]->id = i + 1;
+		philo_one->philo[i]->rfork = philo_one->params->fork[i];
+		philo_one->philo[i]->lfork = philo_one->params->fork[((i + 1) % size)];
+		philo_one->philo[i]->params = philo_one->params;
+		philo_one->philo[i]->ceat = 0;
+		philo_one->philo[i]->last = 0;
+		philo_one->philo[i]->eat = 0;
+		philo_one->philo[i]->stop = 0;
+		i++;
+	}
+	return (0);
+}
+
+t_philo_one		*init(int argc, char **argv)
+{
+	t_philo_one		*tmp;
+
+	if (!(tmp = malloc(sizeof(t_philo_one))))
+		return (NULL);
+	if (init_options(tmp, argc, argv))
+		return (NULL);
+	if (init_philos(tmp, tmp->params->nb_of_philosophers))
+		return (NULL);
+	return (tmp);
 }
