50b3e999ba7a4c0a4f64551d675f3194659ebafc
guysharony <sharonyguy@gmail.com>
2020-10-07 13:33:58 +0200

    Fixing norms

--- philo_one/srcs/scripts/thread.c
+++ philo_one/srcs/scripts/thread.c
@@ -1,40 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guysharony <guysharony@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/07 13:31:09 by guysharony        #+#    #+#             */
+/*   Updated: 2020/10/07 13:32:10 by guysharony       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo_one.h"
 
-void        *sthr(void *philo)
+void		*sthr(void *philo)
 {
-    t_philos    *tmp;
-    pthread_t   tid;
+	t_philos	*tmp;
+	pthread_t	tid;
 
-    tmp = (t_philos*)philo;
-    if (pthread_create(&tid, NULL, &mthread, tmp))
-        return (NULL);
-    pthread_detach(tid);
-    while (1)
-    {
-        if (aeat(tmp))
-            return (NULL);
-        asleep(tmp);
-        athink(tmp);
-    }
-    return (NULL);
+	tmp = (t_philos*)philo;
+	if (pthread_create(&tid, NULL, &mthread, tmp))
+		return (NULL);
+	pthread_detach(tid);
+	while (1)
+	{
+		if (aeat(tmp))
+			return (NULL);
+		asleep(tmp);
+		athink(tmp);
+	}
+	return (NULL);
 }
 
-int         thr(t_philo_one *philo_one)
+int			thr(t_philo_one *philo_one)
 {
-    size_t      i;
-    pthread_t   tid;
+	size_t		i;
+	pthread_t	tid;
 
-    i = 0;
-    philo_one->params->start = ft_time();
-    while (i < philo_one->params->nb_of_philosophers)
-    {
-        philo_one->philo[i]->last = philo_one->params->start;
-        if (pthread_create(&tid, NULL, &sthr, philo_one->philo[i]))
-            return (err("A problem with pthread_create() in \'thread.c\'.", 0));
-        pthread_detach(tid);
-        usleep(100);
-        i++;
-    }
-    mglobal(philo_one);
-    return (0);
+	i = 0;
+	philo_one->params->start = ft_time();
+	while (i < philo_one->params->nb_of_philosophers)
+	{
+		philo_one->philo[i]->last = philo_one->params->start;
+		if (pthread_create(&tid, NULL, &sthr, philo_one->philo[i]))
+			return (err("A problem with pthread_create() in \'thread.c\'.", 0));
+		pthread_detach(tid);
+		usleep(100);
+		i++;
+	}
+	mglobal(philo_one);
+	return (0);
 }
--- philo_one/srcs/scripts/utils.c
+++ philo_one/srcs/scripts/utils.c
@@ -1,20 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guysharony <guysharony@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/07 13:32:16 by guysharony        #+#    #+#             */
+/*   Updated: 2020/10/07 13:33:38 by guysharony       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo_one.h"
 
-size_t      ft_time(void)
+size_t		ft_time(void)
 {
-    struct timeval  time;
+	struct timeval	time;
 
-    gettimeofday(&time, NULL);
-    return (time.tv_sec * 1000 + (time.tv_usec / 1000));
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + (time.tv_usec / 1000));
 }
 
-void        ft_putstr(char *str)
+void		ft_putstr(char *str)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    while(str[i]) {
-        write(1, &str[i], 1);
-        i++;
-    }
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
